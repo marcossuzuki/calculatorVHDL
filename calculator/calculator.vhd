ef66e6ea3467da57126aa1637a96c743a8858b5d
Marcos Suzuki <marcossuzuki@gmail.com>
2018-08-20 09:34:36 -0300

    arquivo comentado

--- calculator/calculator.vhd
+++ calculator/calculator.vhd
@@ -36,7 +36,7 @@ entity calculator is
 end;
 
 architecture struct of calculator is
-	component conv_7seg
+	component conv_7seg	--conversor de binario para display de 7 segmentos
 		port(
 			digit		:		in STD_LOGIC_VECTOR (3 downto 0);
 			enable 	: IN STD_LOGIC;
@@ -44,7 +44,7 @@ architecture struct of calculator is
 		);
 	end component;
 	
-	component PWM is
+	component PWM is --birlho do display de 7 segmentos controlado pelo pwm
 	 generic(n: integer:=8);
 	 port ( 	
 				CLK 		: in  std_logic;
@@ -71,19 +71,19 @@ architecture struct of calculator is
 		);
 	end component;
 
-	component map_ps2	
+	component map_ps2	--conversor dos numeros do numpad para binario, demais teclas são enviadas sem modficacao
 		port( key_in	: in STD_LOGIC_VECTOR (15 downto 0);
 				key_out	: out STD_LOGIC_VECTOR(15 downto 0));
 	end component;
 	
-	component bcd is
+	component bcd is -- conversor binario para binario-decimal
 		generic (n : integer:= 16);
 		Port ( binIN 	 	: in   STD_LOGIC_VECTOR (n-1 downto 0);
 				 bcd_out		: out  STD_LOGIC_VECTOR (n+3 downto 0)
 				);
 	end component;
 	
-	component somador_for IS
+	component somador_for IS -- somador gerado por for loop
 		GENERIC(n 	: INTEGER := 16 );
 		PORT (x, y 	: IN STD_LOGIC_VECTOR (n-1 DOWNTO 0);
 				ze		: IN STD_LOGIC;
@@ -91,7 +91,7 @@ architecture struct of calculator is
 				zs 	: OUT STD_LOGIC);
 	END component;
 	
-	component mult IS
+	component mult IS -- multiplicador, utiliza a operacao de multiplicacao da placa
 		generic (n : integer:= 16);
 		Port ( A, B 	 	: in   STD_LOGIC_VECTOR (n-1 downto 0);
 				result		: out  STD_LOGIC_VECTOR (n-1 downto 0)
@@ -131,6 +131,7 @@ begin
 	
 	resetn <= KEY(0);
 	
+	--port map do display de 7 segmentos
 	hexseg0: conv_7seg port map(
 		toDisplay(3 downto 0), pwm_OUT, HEX0
 	);
@@ -143,39 +144,47 @@ begin
 	hexseg3: conv_7seg port map(
 		toDisplay(15 downto 12), pwm_OUT, HEX3
 	);
-
+	
+	--port map para o teclado ps2
 	kbd_ctrl : kbdex_ctrl generic map(24000) port map(
 		PS2_DAT, PS2_CLK, CLOCK_24(0), KEY(1), resetn, lights,
 		key_on, key_code => key0
 	);
 	
+	--port map do pwm
 	PWM1: pwm PORT MAP (CLK => CLOCK_50, RST => '0', ENABLE => SW(8), DUTY => SW(7 downto 0), pwm_OUT => pwm_OUT);
 	
+	--port map do conversor teclado para binario do numpad de 0-9
 	map1: map_ps2 PORT MAP (key_in=>key0(15 downto 0), key_out=>key_conv);
 	
 	bcd1: bcd PORT MAP (key_seg, bcd_out=> bcd_out);
 	
+	--complemento de 2 para as pilhas satck0 e stack1
 	comp2_stack0 <= not stack0 +1;
 	comp2_stack1 <= not stack1 +1;
 	
+	--operacoes de somma
 	soma1: somador_for PORT MAP (tmp_stack, stack0, ze=>'0', s=>soma_result1, zs=>soma_overflow1);
 	sub1:  somador_for PORT MAP (tmp_stack, comp2_stack0, ze=>'0', s=>sub_result1,  zs=>sub_overflow1);
 	soma2: somador_for PORT MAP (stack0, stack1, ze=>'0', s=>soma_result2, zs=>soma_overflow2);
 	sub2:  somador_for PORT MAP (stack0, comp2_stack1, ze=>'0', s=>sub_result2,  zs=>sub_overflow2);
 	
+	--operacoes de multiplicacao
 	mult1: mult PORT MAP (tmp_stack, stack0, mult_result1);
 	mult2: mult PORT MAP (stack0, stack1, mult_result2);
 	
+	--leds que indicam teclas pressionadas no teclado
 	LEDG(7 downto 5) <= key_on;
 	
 	LEDR(9) <= tmp_stack(15); -- indicador de negativo
 	LEDR(8) <= overflow; --led indicador de overflow
 	
+	--transforma numeros negativos pelo complemento de 2 em positivos para exibicao no display
 	with tmp_stack(15) select
 		key_seg <= not(tmp_stack)+1 when '1',
 					  tmp_stack when others;
 	
-	
+	--atualizacao da maquina de estado do pressionamento das teclas
 	process(CLOCK_50)
 	begin
 		if(CLOCK_50'event and CLOCK_50='1') then
@@ -183,11 +192,12 @@ begin
 		end if;
 	end process;
 
+	--core do funcionamento da calculadora
 	process(key_on(0))
 	begin
 		if(key_on(0) = '1') then
 			if (statek = '0') then
-				case clean_tmp is
+				case clean_tmp is --valores do display precisam ser limpos
 					when '1' =>
 						if (key0(15 downto 0) = x"0066") then
 							tmp_stack <= stack1;
@@ -243,7 +253,7 @@ begin
 								end if;
 								clean_tmp <= '0';
 							elsif(key_conv = x"007C") then --multiplicacao apertando '*' do numpad
-								if((stack0(15) xor stack1(15)) /= mult_result2(15)) then
+								if((stack0(15) xor stack1(15)) /= mult_result2(15)) then --analisa overflow
 									overflow <= '1';
 									stack3 <= (others=>'0');
 									stack2 <= stack3;
@@ -261,11 +271,11 @@ begin
 							end if;
 						end if;	
 						
-					when '0' =>
-						if (key0(15 downto 0) = x"0066") then
+					when '0' => --display nao precisa ser limpo
+						if (key0(15 downto 0) = x"0066") then --backspace para limpar valor atual, e tirar status de overflow
 							tmp_stack <= (others=>'0');
 							overflow <= '0'; --led de overflow
-						elsif (overflow = '0') then
+						elsif (overflow = '0') then	--não pode ocorrer overflow
 							if(key_conv <= x"0009") then
 								if(tmp_stack<x"0CCC") then
 									tmp_stack <= (tmp_stack(14 downto 0) & '0') + (tmp_stack(12 downto 0) & "000") + key_conv; --multiplica por 10 stack0 e soma o valor
@@ -281,7 +291,7 @@ begin
 							elsif(key_conv = x"0077") then --complemento de dois caso aperte NumLcok
 								tmp_stack <= (not tmp_stack)+1;
 							elsif(key_conv = x"0079") then --soma apertando '+' do numpad
-								if((stack0(15) and stack1(15)) /= soma_result1(15)) then
+								if((stack0(15) and stack1(15)) /= soma_result1(15)) then --analisa overflow
 									overflow <= '1';
 									stack0 <= (others=>'0');
 									tmp_stack <=(others=>'0');
@@ -291,7 +301,7 @@ begin
 								end if;
 								clean_tmp <='1';
 							elsif(key_conv = x"007B") then --subtracao apertando '-' do numpad
-								if((stack0(15) and not stack1(15)) /= sub_result1(15)) then
+								if((stack0(15) and not stack1(15)) /= sub_result1(15)) then --analisa overflow
 									overflow <= '1';
 									stack0 <=(others=>'0');
 									tmp_stack <=(others=>'0');
@@ -301,7 +311,7 @@ begin
 								end if;
 								clean_tmp <='1';
 							elsif(key_conv = x"007C") then --multiplicacao apertando '*' do numpad
-								if((tmp_stack(15) xor stack0(15)) /= mult_result1(15)) then
+								if((tmp_stack(15) xor stack0(15)) /= mult_result1(15)) then --analisa overflow
 									overflow <= '1';
 									stack0 <=(others=>'0');
 									tmp_stack <=(others=>'0');
@@ -321,6 +331,7 @@ begin
 		next_statek <= key_on(0);
 	end process;
 	
+	--funcionalidade das telcas <- e -> para movimentacao digitos no display
 	process(CLOCK_50)
 	begin
 		if(key_conv = x"E06B" and bcd_out(19 downto 16)>"0000") then --aperta para esquerda
